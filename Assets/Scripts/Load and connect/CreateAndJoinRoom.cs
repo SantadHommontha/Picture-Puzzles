1d1fb65caec4b638045111de05e793ca60f4b6e0
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-10-03 13:17:59 +0700

    add join server and create and join room script

---
+++ Assets/Scripts/Load and connect/CreateAndJoinRoom.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+{
+    //  [SerializeField] private TMP_InputField createInput;
+    [SerializeField] private TMP_InputField roomInput;
+    [SerializeField] private TMP_Text massage;
+    [SerializeField] private string adminCode = "onion";
+    // [SerializeField] GameObject red;
+    // [SerializeField] private GameObject green;
+
+    void Start()
+    {
+        // PhotonNetwork.AutomaticallySyncScene = true;
+        // PhotonNetwork.EnableCloseConnection = false;
+        // PhotonNetwork.NetworkingClient.LoadBalancingPeer.SerializationProtocolType = ExitGames.Client.Photon.SerializationProtocol.GpBinaryV16;
+        // PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "asia";
+        // PhotonNetwork.ConnectUsingSettings();
+        if (PhotonNetwork.IsConnected)
+        {
+            ChangeMassage("Your Not Connect The Server", false);
+        }
+    }
+
+
+
+    private void ChangeMassage(string _text, bool _clearMassage = true)
+    {
+        if (massage)
+        {
+            massage.text = _text;
+            if (_clearMassage) StartCoroutine(CountDownForClearMassage());
+        }
+    }
+    private IEnumerator CountDownForClearMassage()
+    {
+        yield return new WaitForSeconds(3);
+        ChangeMassage("");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions options = new RoomOptions();
+        options.IsVisible = true;
+        options.IsOpen = true;
+        options.MaxPlayers = 5;
+        PhotonNetwork.CreateRoom(roomInput.text.ToLower(), options);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(roomInput.text.ToLower());
+    }
+
+
+    public void EnterRoomBTN()
+    {
+        if (roomInput.text.ToLower() == adminCode)
+        {
+            CreateRoom();
+        }
+        else
+        {
+            JoinRoom();
+        }
+    }
+    public override void OnCreateRoomFailed(short code, string message)
+    {
+        ChangeMassage($"Create Room Failed");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        ChangeMassage($"Join Room Failed");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        ChangeMassage("Create Room");
+        StartCoroutine(CountDownToLoadScene("Game"));
+    }
+
+    public override void OnJoinedRoom()
+    {
+        ChangeMassage("Joined Room");
+        StartCoroutine(CountDownToLoadScene("Game"));
+    }
+
+
+    private IEnumerator CountDownToLoadScene(string _scenename)
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(_scenename);
+    }
+}
