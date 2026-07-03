d8e9343e9d0a8357eb930f0d0d9cac19df7bf2a3
SantadHommontha <santadhommontha@gmail.com>
2026-07-03 15:43:30 +0700

    add title image

---
+++ Assets/Scripts/ShowTitleImage.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+using UnityEngine.UI;
+
+public class ShowTitleImage : MonoBehaviour
+{
+    [SerializeField] private BoolValue v_showExImage;
+
+    [Space]
+    [SerializeField] private Image image;
+
+    [Space]
+    [SerializeField] private bool invertValue;
+
+
+
+    private void OnValueChange(bool _value)
+    {
+        print($"ggg {_value}");
+        image.enabled = invertValue ? !_value:  _value;
+    }
+
+    private void OnEnable()
+    {
+        v_showExImage.OnValueChange += OnValueChange;
+        
+        OnValueChange(v_showExImage.Value);
+
+
+    }
+
+    private void OnDisable()
+    {
+        v_showExImage.OnValueChange -= OnValueChange;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
